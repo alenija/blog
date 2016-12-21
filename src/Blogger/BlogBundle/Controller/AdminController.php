9442365f5d2fb7ac6dc0ad9eeca3b12dd43ed5e2
alenija <alenija23@gmail.com>
2016-12-21 19:06:23 +0200

    admin.css

--- src/Blogger/BlogBundle/Controller/AdminController.php
+++ src/Blogger/BlogBundle/Controller/AdminController.php
@@ -3,16 +3,24 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $blogs = $this->getDoctrine()->getRepository('BlogBundle:Blog')
             ->getLatestBlogs();
         
-        return $this->render('BlogBundle:Admin:index.html.twig', array(
+        $paginator  = $this->get('knp_paginator');
+
+        $pagination = $paginator->paginate($blogs,
+            $request->query->getInt('page', 1),
+            10);
+        
+        return $this->render('BlogBundle:Admin:index.html.twig', [
             'blogs' => $blogs,
-            ));
+            'pagination' => $pagination,
+            ]);
     }
 }
