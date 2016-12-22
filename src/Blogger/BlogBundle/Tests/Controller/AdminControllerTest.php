7fe51092cc52d8c3328322d23893276b3888299a
alenija <alenija23@gmail.com>
2016-12-22 13:03:05 +0200

    added AdminControllerTest

---
+++ src/Blogger/BlogBundle/Tests/Controller/AdminControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alena
+ * Date: 22.12.16
+ * Time: 12:12
+ */
+
+namespace Blogger\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Blogger\BlogBundle\Entity\User;
+
+class AdminControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $adminName = 'alen.fox';
+
+        $crawler = $client->request('GET', '/admin/',[],[],[
+            'PHP_AUTH_USER' => $adminName,
+            'PHP_AUTH_PW'   => '1111',
+        ]);
+
+//        // выводит ответ
+//        echo $crawler->html();
+
+        $this->assertTrue($crawler->filter('div.admin-title h2:contains("Welcome to the administrator\'s page, '. $adminName .'!")')->count() > 0);
+
+    }
+}
